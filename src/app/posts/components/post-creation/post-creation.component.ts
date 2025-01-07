494dd3314ccb8df3f5d498c2556bdc520ffd41c6
bemoremore <113298256+bemoremore@users.noreply.github.com>
2025-01-07 11:37:55 -0300

    feat: folders

---
+++ src/app/posts/components/post-creation/post-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-creation',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-creation.component.html',
+  styleUrl: './post-creation.component.css'
+})
+export class PostCreationComponent {
+
+}
